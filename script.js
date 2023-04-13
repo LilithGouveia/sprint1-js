4a37514e1c88a482b205991cb0b5ce20be5ae9f1
sabrinafvmorais <RM550781@fiap.com.br>
2023-04-13 18:02:02 -0300

    validar nome adicionado

--- script.js
+++ script.js
@@ -79,6 +79,7 @@ function validarNome() {
     nome.focus();
   }
 }
+//teste
 
 function validarEmail(email){
     let emailFirst = /^[_a-z0-9-]+(.[_a-z0-9-]+)@[a-z0-9-]+(.[a-z0-9-]+)(.[a-z]{2,4})$/;
